e318bce5c864a6695a70db30910f26e142b63ce4
Stephanie Burgos <burgoss@google.com>
2019-04-05 18:04:48 -0400

    while loops practice

---
+++ while.js
@@ -0,0 +1,19 @@
+/* While loops practice */
+
+/* 
+ * Write a while loop that will never run
+*/
+
+
+
+
+
+
+
+/*
+ * Write code that uses a while loop to
+ * count down from 10 to zero and then prints “BOOM!”
+*/
+
+
+
