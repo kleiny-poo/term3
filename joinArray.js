0ca751166ee72cdab37e879312f072cc821a0540
Stephanie Burgos <burgoss@google.com>
2019-04-05 17:42:53 -0400

    Day 1 Arrays Review Activity

---
+++ joinArray.js
@@ -0,0 +1,60 @@
+/* joinArray.js
+ * Follow the prompts to print out 
+ * an interesting quote.
+*/
+
+let phrases = ["enough", "zebras are great!", "are", "think they can", "crazy", "umbrellas"];
+
+/* Sort the phrases array 
+ * in alphabetical order
+*/
+
+
+
+/* Remove the last element in the
+ * phrases array
+*/
+
+
+
+/* Add the string "the ones who do."
+ * to the end of the phrases array
+*/
+
+
+
+/* Add the string "People who" at 
+ * index 0 of the phrases array
+*/
+
+
+
+/* Add the string "to" at 
+ * index 4 of the phrases array
+*/
+
+
+
+/* Replace the string "umbrellas"
+ * with the string at index 1 of the
+ * phrases array.
+ * Hint: Print your array to figure out
+ * the current index of "umbrella".
+*/
+
+
+
+
+/* Add the phrase "change the world," 
+ * at the second to last index of the array.
+*/
+
+
+
+/* This code should print: 
+ * People who are crazy enough to 
+ * think they can change the world, 
+ * are the ones who do. 
+*/
+let quote = phrases.join(" ");
+console.log(quote);
