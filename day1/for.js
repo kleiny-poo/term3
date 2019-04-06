e55ea3d222ca2544b6335ae7a191a036bce86ba5
coach-stephanie <43859479+coach-stephanie@users.noreply.github.com>
2019-04-05 21:18:56 -0400

    Create for.js

---
+++ day1/for.js
@@ -0,0 +1,99 @@
+/* 
+ * for.js
+ * for loop practice with inventors array
+*/
+
+let inventors = [
+	{
+		name: "Mark E. Dean",
+		gender: "Male",
+		invention: "ISA bus, color monitor, gigahertz chip",
+		liked: false
+	},
+	{
+		name: "Charles Richard Drew",
+		gender: "Male",
+		invention: "Blood banks",
+		liked: false
+	},
+	{
+		name: "Madam C.J. Walker",
+		gender: "Female",
+		invention: "African American hair care and cosmetics",
+		liked: false
+	},
+	{
+		name: "Guillermo Gonzalez Camarena",
+		gender: "Male",
+		invention: "Early color television system",
+		liked: false
+	},
+	{
+		name: "Ellen Ochoa",
+		gender: "Female",
+		invention: "Optical analysis systems",
+		liked: false
+	},
+	{
+		name: "Marie Van Brittan Brown",
+		gender: "Female",
+		invention: "Home security systems",
+		liked: false
+	},
+	{
+		name: "Alexander Miles",
+		gender: "Male",
+		invention: "Automatically closing elevator doors",
+		liked: false
+	},
+	{
+		name: "Dr. Patricia Bath",
+		gender: "Female",
+		invention: "Laserphaco Probe",
+		liked: false
+	}
+];
+
+
+/* 
+ * Print all of the inventors in the inventors array,
+ * list their properties
+*/
+console.log("------------------Inventors------------------");
+
+
+
+
+/* 
+ * Print the names of the female inventors
+*/
+console.log("---------------Female Inventors--------------");
+
+
+
+
+
+/* 
+ * Print the names of the male inventors
+*/
+console.log("----------------Male Inventors---------------");
+
+
+
+
+/*
+ * Half of the inventors have been liked on
+ * social media. Assign a value of true to the liked
+ * property of every other inventor. 
+*/
+
+
+
+
+
+/*
+ * Use a loop to count and print the number of liked inventors,
+ * meaning inventors whose liked property is set to true
+*/ 
+
+
